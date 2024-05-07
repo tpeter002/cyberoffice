96b9b82d3a85f2c0b9b279b632d034edf9d58795
Pethő Dominik <pethodominik16@gmail.com>
2024-05-07 21:13:22 +0200

    nem megy mar falba

--- src/env/models/VacuumCleanerModel.java
+++ src/env/models/VacuumCleanerModel.java
@@ -12,8 +12,8 @@ import env.Percept;
 import java.util.ArrayList;
 
 // Human agent environment class
-public class VacuumCleanerModel  {
-	
+public class VacuumCleanerModel {
+
 	private OfficeModel model;
 	private int id;
 	private int GSize;
@@ -37,6 +37,7 @@ public class VacuumCleanerModel  {
 		UP,
 		DOWN,
 	}
+
 	private DIRECTION direction;
 
 	public static final Literal next_slot = Literal.parseLiteral("next_slot");
@@ -53,28 +54,28 @@ public class VacuumCleanerModel  {
 
 	public static final Literal current_room_has_people = Literal.parseLiteral("current_room_has_people");
 	public static final Literal get_location = Literal.parseLiteral("get_location");
-	
+
 	public static final Literal error = Literal.parseLiteral("error");
 	public static final Literal fix = Literal.parseLiteral("fix");
-	
+
 	Random random = new Random(System.currentTimeMillis());
 	private static final double ERROR_PROBABILITY = 0.00;
-	
-	public VacuumCleanerModel(OfficeModel model, int GSize){
+
+	public VacuumCleanerModel(OfficeModel model, int GSize) {
 		this.id = 1;
 		this.model = model;
-		this.homePosition = new Location(0,0);
+		this.homePosition = new Location(0, 0);
 		this.isBroken = false;
 		this.requestedLocation = false;
 		this.garbageSpace = 100;
 		this.batteryLevel = 100;
-		this.lastMove = new Location(0,0);
+		this.lastMove = new Location(0, 0);
 		this.GSize = GSize;
 		this.direction = DIRECTION.RIGHT;
 		initializePositions(GSize);
 	}
 
-	public void initializePositions(int GSize){
+	public void initializePositions(int GSize) {
 		model.setAgPos(this.id, 0, 0);
 		updateRoom();
 	}
@@ -82,44 +83,44 @@ public class VacuumCleanerModel  {
 	public void updateRoom() {
 		this.currentRoom = model.whichRoom(model.getAgPos(this.id).x, model.getAgPos(this.id).y);
 	}
-	
+
 	public ArrayList<Percept> newPercepts() {
-        ArrayList<Percept> percepts = new ArrayList<Percept>();
+		ArrayList<Percept> percepts = new ArrayList<Percept>();
 
 		updateRoom();
 
 		Location vc = model.getAgPos(this.id);
-		
-		if (model.hasGarbage(vc.x, vc.y)) {
-            percepts.add(new Percept(slot_has_garbage));
-        }
 
-		if(this.batteryLevel <= 20 || this.garbageSpace <= 20) {
+		if (model.hasGarbage(vc.x, vc.y)) {
+			percepts.add(new Percept(slot_has_garbage));
+		}
+
+		if (this.batteryLevel <= 20 || this.garbageSpace <= 20) {
 			percepts.add(new Percept(should_go_home));
 		}
 
-		if(model.getAgPos(this.id).equals(homePosition)) {
+		if (model.getAgPos(this.id).equals(homePosition)) {
 			percepts.add(new Percept(at_home));
 		}
-		if(model.getAgPos(this.id).equals(this.whereToCleanNow) && this.roomToClean != null){
+		if (model.getAgPos(this.id).equals(this.whereToCleanNow) && this.roomToClean != null) {
 			percepts.add(new Percept(at_room_to_clean));
 		}
-		
+
 		if (!model.roomIsEmpty(this.currentRoom)) {
 			percepts.add(new Percept(current_room_has_people));
 		}
 
-		if(this.isBroken) {
+		if (this.isBroken) {
 			percepts.add(new Percept(error));
 		}
 
-		if(this.requestedLocation) {
+		if (this.requestedLocation) {
 			percepts.add(new Percept(Literal.parseLiteral("location(" + vc.x + ", " + vc.y + ")")));
 			this.requestedLocation = false;
-		} 
+		}
 
 		return percepts;
-    }
+	}
 
 	public ArrayList<Percept> perceptsToRemove() {
 		ArrayList<Percept> percepts = new ArrayList<Percept>();
@@ -127,112 +128,112 @@ public class VacuumCleanerModel  {
 		Location vc = model.getAgPos(this.id);
 
 		if (!model.hasGarbage(vc.x, vc.y)) {
-            percepts.add(new Percept(slot_has_garbage));
-        }
+			percepts.add(new Percept(slot_has_garbage));
+		}
 
-		if(!(this.batteryLevel <= 20 || this.garbageSpace <= 20)){
+		if (!(this.batteryLevel <= 20 || this.garbageSpace <= 20)) {
 			percepts.add(new Percept(should_go_home));
 		}
 
-		if(!model.getAgPos(this.id).equals(homePosition)) {
+		if (!model.getAgPos(this.id).equals(homePosition)) {
 			percepts.add(new Percept(at_home));
 		}
-		
+
 		if (model.roomIsEmpty(this.currentRoom)) {
 			percepts.add(new Percept(current_room_has_people));
 		}
 
-		if(!this.isBroken){
+		if (!this.isBroken) {
 			percepts.add(new Percept(error));
 		}
 
 		return percepts;
 	}
 
-	public void executeAction(Structure action){
-        try {
+	public void executeAction(Structure action) {
+		try {
 
-            if (action.equals(next_slot)) {
+			if (action.equals(next_slot)) {
 				if (!model.roomIsEmpty(this.currentRoom)) {
 					System.out.println("Room is not empty");
 					moveTowards(homePosition);
-				}
-				else if (this.currentRoom==OfficeModel.ROOM.DOORWAY){
+				} else if (this.currentRoom == OfficeModel.ROOM.DOORWAY) {
 					cleanCurrentRoom();
 					System.out.println("ajtoban vagyok");
-				}
-				else{
+				} else {
 					cleanCurrentRoom();
 				}
-            }
-			else if(action.getFunctor().equals("go_to")) {
-				int x = ((int)((NumberTerm)action.getTerm(0)).solve());
+			} else if (action.getFunctor().equals("go_to")) {
+				int x = ((int) ((NumberTerm) action.getTerm(0)).solve());
 				System.out.println("funktoros geci");
 				System.out.println(x);
 				goTowardSelectedRoom(OfficeModel.ROOM.values()[x]);
-			}
-			else if (action.equals(pick_garbage)) {
-                pickGarbage();
-			}
-			else if (action.equals(move_home)) {
-                moveTowards(homePosition);
-			}
-			else if (action.equals(empty_garbage)) {
+			} else if (action.equals(pick_garbage)) {
+				pickGarbage();
+			} else if (action.equals(move_home)) {
+				moveTowards(homePosition);
+			} else if (action.equals(empty_garbage)) {
 				empty_garbage();
-			}
-			else if (action.equals(recharge_battery)) {
+			} else if (action.equals(recharge_battery)) {
 				recharge_battery();
-			}
-			else if (action.equals(get_location)) {
+			} else if (action.equals(get_location)) {
 				get_location();
-			}
-			else if (action.equals(fix)) {
+			} else if (action.equals(fix)) {
 				fix();
 			}
-			
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	public void goTowardSelectedRoom(OfficeModel.ROOM room){
+	public void goTowardSelectedRoom(OfficeModel.ROOM room) {
 		System.out.println("bejutottam a go towardba");
 		this.roomToClean = room;
 		Location vc = model.getAgPos(this.id);
 		Location next = vc;
-		Location door = model.getDoorwayPos(room ,model.whichRoom(vc.x, vc.y));
+		Location door = model.getDoorwayPos(room, model.whichRoom(vc.x, vc.y));
 		this.whereToCleanNow = door;
-		if (vc.x < door.x) next.x = vc.x + 1;
-		if (vc.x > door.x) next.x = vc.x - 1;
-		if (vc.y < door.y) next.y = vc.y + 1;
-		if (vc.y > door.y) next.y = vc.y - 1;
+		if (vc.x < door.x && !model.isWall(vc.x + 1, vc.y))
+			next.x = vc.x + 1;
+		if (vc.x > door.x && !model.isWall(vc.x - 1, vc.y))
+			next.x = vc.x - 1;
+		if (vc.y < door.y && !model.isWall(vc.x, vc.y+1))
+			next.y = vc.y + 1;
+		if (vc.y > door.y && !model.isWall(vc.x, vc.y-1))
+			next.y = vc.y - 1;
 		model.setAgPos(this.id, next);
 	}
-	
+
 	public void moveTowards(Location loc) {
 		Location vc = model.getAgPos(this.id);
 		Location next = vc;
-		//boolean inSameRoom = model.whichRoom(vc.x, vc.y) == model.whichRoom(loc.x, loc.y);
-		//boolean inDoorway = this.currentRoom == OfficeModel.ROOM.DOORWAY;
+		// boolean inSameRoom = model.whichRoom(vc.x, vc.y) == model.whichRoom(loc.x,
+		// loc.y);
+		// boolean inDoorway = this.currentRoom == OfficeModel.ROOM.DOORWAY;
 		// if (!inSameRoom) {
-		// 	Location door = model.getDoorwayPos(model.whichRoom(loc.x, loc.y));
-		// 	loc.x = door.x;
-		// 	loc.y = door.y;
+		// Location door = model.getDoorwayPos(model.whichRoom(loc.x, loc.y));
+		// loc.x = door.x;
+		// loc.y = door.y;
 		// }
 		// if (inDoorway) {
-		// 	loc.x = lastMove.x + vc.x;
-		// 	loc.y = lastMove.y + vc.y;
+		// loc.x = lastMove.x + vc.x;
+		// loc.y = lastMove.y + vc.y;
 		// }
 
-		int dx = Math.abs(loc.x-vc.x);
-		int dy = Math.abs(loc.y-vc.y);
-			
+		int dx = Math.abs(loc.x - vc.x);
+		int dy = Math.abs(loc.y - vc.y);
+
 		if (dx > dy) {
-			if (vc.x < loc.x) next.x = vc.x + 1;
-			if (vc.x > loc.x) next.x = vc.x - 1;
+			if (vc.x < loc.x)
+				next.x = vc.x + 1;
+			if (vc.x > loc.x)
+				next.x = vc.x - 1;
 		} else {
-			if (vc.y < loc.y) next.y = vc.y + 1;
-			if (vc.y > loc.y) next.y = vc.y - 1;
+			if (vc.y < loc.y)
+				next.y = vc.y + 1;
+			if (vc.y > loc.y)
+				next.y = vc.y - 1;
 		}
 
 		if (model.isWall(next.x, vc.y)) {
@@ -242,11 +243,11 @@ public class VacuumCleanerModel  {
 			next.y = vc.y;
 		}
 
-		//this.lastMove.x = next.x - vc.x;
-		//this.lastMove.y = next.y - vc.y;
-		
+		// this.lastMove.x = next.x - vc.x;
+		// this.lastMove.y = next.y - vc.y;
+
 		model.setAgPos(this.id, next);
-		
+
 	}
 
 	public void pickGarbage() {
@@ -254,25 +255,24 @@ public class VacuumCleanerModel  {
 		this.garbageSpace -= 5;
 		this.batteryLevel -= 5;
 		model.removeGarbage(vc.x, vc.y);
-	}	
+	}
 
 	void cleanCurrentRoom() {
 		Location vc = model.getAgPos(this.id);
-		if (this.direction == DIRECTION.RIGHT){
+		if (this.direction == DIRECTION.RIGHT) {
 			vc.x++;
 			avoidHumans(vc);
 			avoidHumans(vc);
-			if (this.GSize<=vc.x || model.isWall(vc.x, vc.y)) {
+			if (this.GSize <= vc.x || model.isWall(vc.x, vc.y)) {
 				this.direction = DIRECTION.LEFT;
 				vc.x--;
 				vc.y++;
-				if(model.isWall(vc.x, vc.y) || !(model.inGrid(vc.x, vc.y))){
+				if (model.isWall(vc.x, vc.y) || !(model.inGrid(vc.x, vc.y))) {
 					vc.y--;
 					avoidHumans(vc);
 				}
 			}
-		}
-		else if (this.direction == DIRECTION.LEFT){
+		} else if (this.direction == DIRECTION.LEFT) {
 			vc.x--;
 			avoidHumans(vc);
 			avoidHumans(vc);
@@ -280,7 +280,7 @@ public class VacuumCleanerModel  {
 				this.direction = DIRECTION.RIGHT;
 				vc.x++;
 				vc.y++;
-				if (model.isWall(vc.x, vc.y) || !(model.inGrid(vc.x, vc.y))){
+				if (model.isWall(vc.x, vc.y) || !(model.inGrid(vc.x, vc.y))) {
 					vc.y--;
 					avoidHumans(vc);
 				}
@@ -299,24 +299,26 @@ public class VacuumCleanerModel  {
 			if (model.cellOccupied(vc.x, vc.y)) {
 				System.out.println("Human detected, moving away...");
 				if (this.direction == DIRECTION.RIGHT) {
-					if (model.inGrid(vc.x, vc.y - 1) && !model.isWall(vc.x, vc.y - 1) && !model.cellOccupied(vc.x, vc.y - 1)) {
+					if (model.inGrid(vc.x, vc.y - 1) && !model.isWall(vc.x, vc.y - 1)
+							&& !model.cellOccupied(vc.x, vc.y - 1)) {
 						// Move up
 						vc.y--;
 						model.setAgPos(this.id, vc);
-					}
-					else if (model.inGrid(vc.x, vc.y + 1) && !model.isWall(vc.x, vc.y + 1) && !model.cellOccupied(vc.x, vc.y + 1)) {
+					} else if (model.inGrid(vc.x, vc.y + 1) && !model.isWall(vc.x, vc.y + 1)
+							&& !model.cellOccupied(vc.x, vc.y + 1)) {
 						// Move down
 						vc.y++;
 						model.setAgPos(this.id, vc);
-						
+
 					}
 				} else if (this.direction == DIRECTION.LEFT) {
-					if (model.inGrid(vc.x, vc.y - 1) && !model.isWall(vc.x, vc.y - 1) && !model.cellOccupied(vc.x, vc.y - 1)) {
+					if (model.inGrid(vc.x, vc.y - 1) && !model.isWall(vc.x, vc.y - 1)
+							&& !model.cellOccupied(vc.x, vc.y - 1)) {
 						// Move up
 						vc.y--;
 						model.setAgPos(this.id, vc);
-					}
-					else if (model.inGrid(vc.x, vc.y + 1) && !model.isWall(vc.x, vc.y + 1) && !model.cellOccupied(vc.x, vc.y + 1)) {
+					} else if (model.inGrid(vc.x, vc.y + 1) && !model.isWall(vc.x, vc.y + 1)
+							&& !model.cellOccupied(vc.x, vc.y + 1)) {
 						// Move down
 						vc.y++;
 						model.setAgPos(this.id, vc);
@@ -326,11 +328,11 @@ public class VacuumCleanerModel  {
 		}
 	}
 
-	private void empty_garbage(){
+	private void empty_garbage() {
 		this.garbageSpace = 100;
 	}
 
-	private void recharge_battery(){
+	private void recharge_battery() {
 		this.batteryLevel = 100;
 	}
 
@@ -340,6 +342,6 @@ public class VacuumCleanerModel  {
 
 	public void fix() {
 		this.isBroken = false;
-	}	
+	}
 
 }
